760b7baba71cc65bc02112b784b2c2905af836fa
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-12 11:35:38 +0700

    Update code

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,14 @@ import Todo from "./components/Todo";
 
 export default function App() {
   return (
-    <div>
-      <Todo />
+    <div className="hero min-h-screen" style={{
+        backgroundImage:
+          "url(https://static0.polygonimages.com/wordpress/wp-content/uploads/2025/09/chainsaw-man.jpg?w=1600&h=1600&fit=crop)",
+      }}>
+      <div className="hero-overlay"></div>
+      <div className="hero-content text-neutral-content text-center">
+        <Todo />
+      </div>
     </div>
   )
 }
