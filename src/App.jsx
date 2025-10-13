56a38ce7cacb2ea303fabb2412f080480000c79a
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-13 08:01:16 +0700

    Update Code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Todo from "./components/Todo";
+import LogoCSMRezeArc from "./assets/LogoCSMRezeArc.png";
 
 export default function App() {
   return (
-    <div className="hero min-h-screen" style={{
-        backgroundImage:
-          "url(https://static0.polygonimages.com/wordpress/wp-content/uploads/2025/09/chainsaw-man.jpg?w=1600&h=1600&fit=crop)",
-      }}>
-      <div className="hero-overlay"></div>
-      <div className="hero-content text-neutral-content text-center">
+    <div className="hero min-h-screen relative" style={{backgroundImage: "url(https://static0.polygonimages.com/wordpress/wp-content/uploads/2025/09/chainsaw-man.jpg?w=1600&h=1600&fit=crop)", backgroundSize: "cover", backgroundPosition: "center",}}>
+      {/* Overlay Blur */}
+      <div className="hero-overlay backdrop-blur-md absolute inset-0"></div>
+      {/* Logo di depan background */}
+      <img src={LogoCSMRezeArc} alt="Logo CSM Reze Arc" className="z-10"/>
+      <div className="hero-content text-neutral-content text-center relative z-10">
         <Todo />
       </div>
     </div>
-  )
+  );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import {FaPlus, FaTrash, FaCheck} from 'react-icons/fa';
+import LogoCSMRezeArc from "../assets/LogoCSMRezeArc.png";
 
 export default function Todo() {
   return (
