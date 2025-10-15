ac870348225fa568efa4c3f9eca671bd61e05d60
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-15 08:49:23 +0700

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Todo from "./components/Todo";
+import BackgroundCSM from './assets/BackgroundCSM.jpg'
 import LogoCSMRezeArc from "./assets/LogoCSMRezeArc.png";
 
 export default function App() {
   return (
-    <div className="hero min-h-screen relative" style={{backgroundImage: "url(https://static0.polygonimages.com/wordpress/wp-content/uploads/2025/09/chainsaw-man.jpg?w=1600&h=1600&fit=crop)", backgroundSize: "cover", backgroundPosition: "center",}}>
-      {/* Overlay Blur */}
-      <div className="hero-overlay backdrop-blur-md absolute inset-0"></div>
-      {/* Logo di depan background */}
-      <img src={LogoCSMRezeArc} alt="Logo CSM Reze Arc" className="z-10"/>
-      <div className="hero-content text-neutral-content text-center relative z-10">
+    <div className="hero min-h-screen" style={{backgroundImage: `url(${BackgroundCSM})`}}>
+      <div className="hero-overlay backdrop-blur-sm" />
+      <img src={LogoCSMRezeArc} alt="Logo CSM Reze Arc" className="z-10" />
+      <div className="max-w-md w-11/12 z-10">
         <Todo />
       </div>
     </div>
