88fddf40ca6c2e64bf9d8470254247c6e3d55470
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-15 08:54:27 +0700

    Update style

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,7 +4,7 @@ import LogoCSMRezeArc from "../assets/LogoCSMRezeArc.png";
 
 export default function Todo() {
   return (
-    <div className="bg-black/30 text-white place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-xl border border-black/20 shadow-lg shadow-gray">
+    <div className="bg-black/60 text-white place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-sm border border-black/20 shadow-lg shadow-gray">
       <h1>Todo List V3</h1>
     </div>
   )
