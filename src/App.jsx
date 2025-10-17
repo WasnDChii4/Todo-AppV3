c360073eb748428c8a3f4ac451f4244fde0e2bb8
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-17 08:52:50 +0700

    new style

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
     <div className="hero min-h-screen" style={{backgroundImage: `url(${BackgroundCSM})`}}>
       <div className="hero-overlay backdrop-blur-sm" />
       <img src={LogoCSMRezeArc} alt="Logo CSM Reze Arc" className="z-10" />
-      <div className="max-w-md w-11/12 z-10">
+      <div className="w-11/12 z-10">
         <Todo />
       </div>
     </div>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,7 +5,9 @@ import LogoCSMRezeArc from "../assets/LogoCSMRezeArc.png";
 export default function Todo() {
   return (
     <div className="bg-black/60 text-white place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-sm border border-black/20 shadow-lg shadow-gray">
-      <h1>Todo List V3</h1>
+      <div className="bg-gradient-to-r from-red-700/80 to-zinc-900/80 p-4 rounded-xl backdrop-blur-lg mb-6">
+        <h1 className="font-saira-condensed text-3xl font-bold">Todo List V3</h1>
+      </div>
     </div>
   )
 }
