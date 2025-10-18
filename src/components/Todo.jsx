6a07e9d8e88a83dd6d57912fe8901b9c73edc004
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-18 16:29:42 +0700

    update code

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {FaFire, FaTrash, FaCheck} from 'react-icons/fa';
+import {FaFire, FaBomb, FaExplosion, FaCheck} from 'react-icons/fa6';
 
 export default function Todo() {
   const [todos, setTodos] = useState([
@@ -27,8 +27,16 @@ export default function Todo() {
     setNewTodo('')
   }
 
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
+  const completedTodo = (id) => {
+    setTodos(todos.map(todo => todo.id == id ? {...todo, completed: !todo.completed} : todo));
+  }
+
   return (
-    <div className="bg-zinc-800/60 text-white place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-sm border border-zinc-900/20 shadow-lg shadow-gray">
+    <div className="bg-zinc-900/60 text-white place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-sm border border-zinc-900/20 shadow-lg shadow-gray">
       <div className="bg-gradient-to-r from-red-700/80 to-zinc-900/80 p-4 rounded-xl backdrop-blur-sm mb-6">
         <h1 className="font-saira-condensed text-3xl font-bold">Todo List V3</h1>
       </div>
@@ -40,10 +48,35 @@ export default function Todo() {
             onChange={(e) => setNewTodo(e.target.value)}
             onKeyPress={(e) => e.key === 'Enter' && addTodo()}
           />
-          <button className="bg-red-700/80 hover:bg-rose-900/80 px-4 py-3 backdrop-blur-md transition flex items-center justify-center">
+          <button className="bg-red-700/80 hover:bg-rose-900/80 px-4 py-3 backdrop-blur-md transition flex items-center justify-center" onClick={addTodo}>
             <FaFire />
           </button>
         </div>
+      </div>
+
+      <div className="space-y-3">
+        {
+          todos.map((todo) => (
+            <div key={todo.id} className="p-4 rounded-xl bg-neutral-100/80 backdrop-blur-xl border border-black/30">
+              <div className="flex justify-between items-start">
+                <div className="flex items-start space-x-3">
+                  <button className={`${todo.completed ? 'bg-red-700' : 'bg-zinc-900'} mt-1 p-2 rounded-md backdrop-blur-sm border border-black/30 transition`} onClick={() => completedTodo(todo.id)}>
+                    {todo.completed && <FaCheck size={12} />}
+                  </button>
+                  <div>
+                    <p className={`font-medium ${todo.completed ? 'line-through text-red-700' : 'text-zinc-900'} font-chicle text-2xl transition`}>
+                      {todo.text}
+                    </p>
+                    <p className="text-xs text-red-700/80 mt-1">{todo.time} | {todo.date}</p>
+                  </div>
+                </div>
+                <button className="bg-red-700 backdrop-blur-sm border-rose-900/30 transition p-2 rounded-lg" onClick={() => deleteTodo(todo.id)}>
+                  <FaBomb />
+                </button>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
