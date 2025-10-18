abbc44d7a803f8944eef8e4b79bb74b6d1c68b03
WAHYU AJI SETYO NUGROHO <wahyuajisn@gmail.com>
2025-10-18 08:37:11 +0700

    New Style

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -28,9 +28,12 @@ export default function Todo() {
   }
 
   return (
-    <div className="bg-black/60 text-white place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-sm border border-black/20 shadow-lg shadow-gray">
+    <div className="bg-zinc-900/60 text-white place-self-center w-11/12 max-w-md flex flex-col p-3 md:p-7 rounded-xl backdrop-blur-sm border border-zinc-900/20 shadow-lg shadow-gray">
       <div className="bg-gradient-to-r from-red-700/80 to-zinc-900/80 p-4 rounded-xl backdrop-blur-lg mb-6">
         <h1 className="font-saira-condensed text-3xl font-bold">Todo List V3</h1>
+      </div>
+      <div className="mb-6">
+        <h1 className="font-nunito-sans text-lg font-semibold mb-6">Add Task</h1>
       </div>
     </div>
   )
